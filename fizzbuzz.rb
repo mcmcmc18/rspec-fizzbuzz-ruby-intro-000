b79c8c9f863abdfcaa4c756ef49da443289499bd
beta-proxy-8036 <mariacristina.simoes@gmail.com>
2018-10-25 01:06:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative './spec_helper.rb'
-require_relative '../fizzbuzz_spec.rb'
 
 def fizzbuzz(num)
   if (num % 5 == 0) && (num % 3 == 0)
@@ -14,13 +12,3 @@ def fizzbuzz(num)
     return
   end
 end
-
-#def fizzbuzz(int)
-# if (int % 3 == 0) && (int % 5 == 0)
- #return "FizzBuzz"
-#  elsif int % 3 == 0 # if the number int is divisible by 3
- #   return "Fizz" # Go fizz
-#  elsif int % 5 == 0 # if the number int is divisible by 3
-#    return"Buzz" # Go fizz
- # end
-#end
