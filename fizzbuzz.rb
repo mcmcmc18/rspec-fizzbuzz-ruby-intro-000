345e1b9760dc5e9d0036e08144e9225a82dae3fd
beta-proxy-8036 <mariacristina.simoes@gmail.com>
2018-10-25 00:57:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 require_relative './spec_helper.rb'
-require_relative '../fizzbuzz.rb'
+require_relative '../fizzbuzz_spec.rb'
 def fizzbuzz(int)
  if int % 3 == 0 && int % 5 == 0 
  "FizzBuzz"
