3761ad7bb1728aa4238635e3a57b4f9c65da6a25
beta-proxy-8036 <mariacristina.simoes@gmail.com>
2018-10-25 00:41:21 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,6 +3,7 @@ require_relative '../fizzbuzz.rb'
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 
 def fizzbuzz
+end
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
