39ec5ccf7edd092f103cb1bd0480547e12093bb6
beta-proxy-8036 <mariacristina.simoes@gmail.com>
2018-10-25 00:53:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,11 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz_spec.rb'
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    return "Fizz" # Go fizz
+ if int % 3 == 0 && int % 5 == 0 
+ "FizzBuzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  elsif int % 5 == 0 # if the number int is divisible by 3
+    "Buzz" # Go fizz
   end
- 
-fizzbuzz(3)
+end
